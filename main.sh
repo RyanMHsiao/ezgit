4358ba40ece47f60fbc4ace1f03cbd4b97cd35ed
RyanMHsiao <109319635+RyanMHsiao@users.noreply.github.com>
2025-05-20 20:46:30 -0700

    First attempt of error checking after forwarding push to git.

--- main.sh
+++ main.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-echo "You are using ezgit"
-git "$@"
+case "$1" in
+	"push")
+		echo "ezgit: Forwarding push to git"
+		output=$(git "$@")
+		if [ $? -ne 1 ]; then
+			echo "debug: exit status $?"
+			if echo "$output" | grep -q "Permission denied (publickey)"; then
+				echo "You may have forgotten to set your SSH key at https://github.com/settings/keys"
+			fi
+		fi
+		;;
+	*)
+		echo "ezgit: Forwarding command to git"
+		git "$@"
+esac
